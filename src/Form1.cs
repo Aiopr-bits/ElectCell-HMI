df77a95a6bab39c6a940a559553f6199ee2d0a35
曾志勇 <mailzengzhiyong@gmail.com>
2024-10-11 16:27:26 +0800

    feat:初始文件提交

---
+++ src/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ElectCell_HMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (File.Exists(@".\ElectCell-HMI.exe") && File.Exists(@"..\..\..\ElectCell-HMI.exe"))
+            {
+                File.Copy(@".\ElectCell-HMI.exe", @"..\..\..\ElectCell-HMI.exe", true);
+            }
+
+
+
+        }
+    }
+}
