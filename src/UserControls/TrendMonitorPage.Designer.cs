e33b82a9990be98a9b6b032ad8fe57c926f835b6
曾志勇 <mailzengzhiyong@gmail.com>
2024-10-16 09:45:03 +0800

    feat:趋势监控页面调整

--- src/UserControls/TrendMonitorPage.Designer.cs
+++ src/UserControls/TrendMonitorPage.Designer.cs
@@ -30,18 +30,26 @@
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(3, 454);
+            this.richTextBox1.Location = new System.Drawing.Point(3, 504);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(788, 144);
+            this.richTextBox1.Size = new System.Drawing.Size(788, 94);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
@@ -50,25 +58,73 @@
             this.tableLayoutPanel1.AutoScroll = true;
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(794, 601);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox4, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox2, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox3, 0, 3);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(788, 495);
+            this.tableLayoutPanel2.TabIndex = 1;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(3, 23);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(788, 445);
-            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.Size = new System.Drawing.Size(388, 221);
+            this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(397, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(388, 221);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 270);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(388, 222);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox4.Location = new System.Drawing.Point(397, 270);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(388, 222);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
             // 
             // TrendMonitorPage
             // 
@@ -78,7 +134,11 @@
             this.Name = "TrendMonitorPage";
             this.Size = new System.Drawing.Size(794, 601);
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -87,6 +147,10 @@
 
         public System.Windows.Forms.RichTextBox richTextBox1;
         public System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- src/UserControls/TrendMonitorPage.cs
+++ src/UserControls/TrendMonitorPage.cs
@@ -13,45 +13,78 @@ namespace ElectCell_HMI
     public partial class TrendMonitorPage : UserControl
     {
         private MainWindow mainWindow;
-        private List<PointF> dataPoints;
+        private List<PointF> dataPoints1;
+        private List<PointF> dataPoints2;
+        private List<PointF> dataPoints3;
+        private List<PointF> dataPoints4;
 
         public TrendMonitorPage(MainWindow mainWindow)
         {
             InitializeComponent();
             this.mainWindow = mainWindow;
             this.mainWindow.TimerTicked += MainWindow_TimerTicked;
-            this.Resize += TrendMonitorPage_Resize; // 添加Resize事件处理程序
-            dataPoints = new List<PointF>();
+            this.Resize += TrendMonitorPage_Resize; 
+            dataPoints1 = new List<PointF>();
+            dataPoints2 = new List<PointF>();
+            dataPoints3 = new List<PointF>();
+            dataPoints4 = new List<PointF>();
         }
 
         private void MainWindow_TimerTicked(object sender, EventArgs e)
         {
-            // 清空现有数据点
-            dataPoints.Clear();
-
-            // 假设 Data.result.result 是一个二维数组或列表
+            dataPoints1.Clear();
             for (int i = 0; i < Data.result.result.Count; i++)
             {
                 float x = (float)Data.result.result[i][0];
                 float y = (float)Data.result.result[i][1];
-                dataPoints.Add(new PointF(x, y));
+                dataPoints1.Add(new PointF(x, y));
             }
+            DrawGraph(dataPoints1,pictureBox1);
 
-            // 重新绘制图形
-            DrawGraph();
+            dataPoints2.Clear();
+            for (int i = 0; i < Data.result.result.Count; i++)
+            {
+                float x = (float)Data.result.result[i][0];
+                float y = (float)Data.result.result[i][2];
+                dataPoints2.Add(new PointF(x, y));
+            }
+            DrawGraph(dataPoints2, pictureBox2);
+
+            dataPoints3.Clear();
+            for (int i = 0; i < Data.result.result.Count; i++)
+            {
+                float x = (float)Data.result.result[i][0];
+                float y = (float)Data.result.result[i][3];
+                dataPoints3.Add(new PointF(x, y));
+            }
+            DrawGraph(dataPoints3, pictureBox3);
+
+            //添加一个随时间变化的正交曲线
+            dataPoints4.Clear();
+            for (int i = 0; i < Data.result.result.Count; i++)
+            {
+                float x = (float)Data.result.result[i][0]/100;
+                float y = (float)Math.Sin(x);
+                dataPoints4.Add(new PointF(x, y));
+            }
+            DrawGraph(dataPoints4, pictureBox4);
+
         }
 
         private void TrendMonitorPage_Resize(object sender, EventArgs e)
         {
-            DrawGraph();
+            DrawGraph(dataPoints1, pictureBox1);
+            DrawGraph(dataPoints2, pictureBox2);
+            DrawGraph(dataPoints3, pictureBox3);
+            DrawGraph(dataPoints4, pictureBox4);
         }
 
-        private void DrawGraph()
+        private void DrawGraph(List<PointF> dataPoints, System.Windows.Forms.PictureBox pictureBox)
         {
-            if (pictureBox1.Width == 0 || pictureBox1.Height == 0|| dataPoints.Count == 0)
+            if (pictureBox.Width == 0 || pictureBox.Height == 0 || dataPoints.Count == 0)
                 return;
 
-            Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
             using (Graphics g = Graphics.FromImage(bitmap))
             {
                 g.Clear(Color.White);
@@ -62,24 +95,31 @@ namespace ElectCell_HMI
                 float minY = dataPoints.Min(p => p.Y);
                 float maxY = dataPoints.Max(p => p.Y);
 
+                // 如果 minY 和 maxY 相等，设置一个默认范围
+                if (minY == maxY)
+                {
+                    minY -= 1;
+                    maxY += 1;
+                }
+
                 // 设置坐标轴
                 Pen axisPen = new Pen(Color.Black, 2);
-                g.DrawLine(axisPen, 50, 10, 50, pictureBox1.Height - 50); // Y轴
-                g.DrawLine(axisPen, 50, pictureBox1.Height - 50, pictureBox1.Width - 10, pictureBox1.Height - 50); // X轴
+                g.DrawLine(axisPen, 50, 10, 50, pictureBox.Height - 50); // Y轴
+                g.DrawLine(axisPen, 50, pictureBox.Height - 50, pictureBox.Width - 10, pictureBox.Height - 50); // X轴
 
                 // 绘制网格线和坐标标签
                 Pen gridPen = new Pen(Color.LightGray, 1);
                 Font labelFont = new Font("Arial", 8);
-                for (int i = 50; i < pictureBox1.Width - 10; i += 40) // 增加间隔
+                for (int i = 50; i < pictureBox.Width - 10; i += 40) // 增加间隔
                 {
-                    g.DrawLine(gridPen, i, 10, i, pictureBox1.Height - 50);
-                    float xValue = minX + (i - 50) / (float)(pictureBox1.Width - 60) * (maxX - minX);
-                    g.DrawString(xValue.ToString("0.0"), labelFont, Brushes.Black, new PointF(i, pictureBox1.Height - 45));
+                    g.DrawLine(gridPen, i, 10, i, pictureBox.Height - 50);
+                    float xValue = minX + (i - 50) / (float)(pictureBox.Width - 60) * (maxX - minX);
+                    g.DrawString(xValue.ToString("0.0"), labelFont, Brushes.Black, new PointF(i, pictureBox.Height - 45));
                 }
-                for (int i = 10; i < pictureBox1.Height - 50; i += 20)
+                for (int i = 10; i < pictureBox.Height - 50; i += 20)
                 {
-                    g.DrawLine(gridPen, 50, i, pictureBox1.Width - 10, i);
-                    float yValue = maxY - (i - 10) / (float)(pictureBox1.Height - 60) * (maxY - minY);
+                    g.DrawLine(gridPen, 50, i, pictureBox.Width - 10, i);
+                    float yValue = maxY - (i - 10) / (float)(pictureBox.Height - 60) * (maxY - minY);
                     g.DrawString(yValue.ToString("0.0"), labelFont, Brushes.Black, new PointF(5, i - 5));
                 }
 
@@ -89,24 +129,29 @@ namespace ElectCell_HMI
                     Pen dataPen = new Pen(Color.Blue, 2);
                     for (int i = 1; i < dataPoints.Count; i++)
                     {
-                        PointF p1 = new PointF(50 + (dataPoints[i - 1].X - minX) / (maxX - minX) * (pictureBox1.Width - 60),
-                                               pictureBox1.Height - 50 - (dataPoints[i - 1].Y - minY) / (maxY - minY) * (pictureBox1.Height - 60));
-                        PointF p2 = new PointF(50 + (dataPoints[i].X - minX) / (maxX - minX) * (pictureBox1.Width - 60),
-                                               pictureBox1.Height - 50 - (dataPoints[i].Y - minY) / (maxY - minY) * (pictureBox1.Height - 60));
+                        PointF p1 = new PointF(50 + (dataPoints[i - 1].X - minX) / (maxX - minX) * (pictureBox.Width - 60),
+                                               pictureBox.Height - 50 - (dataPoints[i - 1].Y - minY) / (maxY - minY) * (pictureBox.Height - 60));
+                        PointF p2 = new PointF(50 + (dataPoints[i].X - minX) / (maxX - minX) * (pictureBox.Width - 60),
+                                               pictureBox.Height - 50 - (dataPoints[i].Y - minY) / (maxY - minY) * (pictureBox.Height - 60));
                         g.DrawLine(dataPen, p1, p2);
                     }
                 }
 
                 // 绘制标题
-                Font titleFont = new Font("Arial", 14, FontStyle.Bold);
-                g.DrawString("二维曲线图", titleFont, Brushes.Black, new PointF(pictureBox1.Width / 2 - 50, 10));
+                //Font titleFont = new Font("Arial", 14, FontStyle.Bold);
+                //g.DrawString("", titleFont, Brushes.Black, new PointF(pictureBox.Width / 2 - 50, 10));
 
                 // 绘制坐标轴标签
-                g.DrawString("X轴", labelFont, Brushes.Black, new PointF(pictureBox1.Width - 30, pictureBox1.Height - 40));
-                g.DrawString("Y轴", labelFont, Brushes.Black, new PointF(10, 20));
+                //g.DrawString("X轴", labelFont, Brushes.Black, new PointF(pictureBox.Width - 30, pictureBox.Height - 40));
+                //g.DrawString("Y轴", labelFont, Brushes.Black, new PointF(10, 20));
             }
 
-            pictureBox1.Image = bitmap;
+            pictureBox.Image = bitmap;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
